f2a7c6bdcf3deb9d02f89240a791bfd193e94d6f
Garry Banks <ellisbanks24@gmail.com>
2022-02-01 18:54:56 -0500

    Add project files.

---
+++ ChildcareScouter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChildcareScouter.MvcApplication" Language="C#" %>
---
+++ ChildcareScouter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChildcareScouter.Startup))]
+namespace ChildcareScouter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
